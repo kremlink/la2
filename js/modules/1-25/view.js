88027130bc8a701ebade20e52647b46b64c00731
Sasha <kremlink0@gmail.com>
2021-08-02 22:48:40 +0300

    packing

--- js/modules/1-25/view.js
+++ js/modules/1-25/view.js
@@ -27,6 +27,7 @@ export let PackingView=BaseIntView.extend({
  iLength:null,
  phase2Lotties:[],
  circles:{clicked:0,failed:0},
+ points:0,
  initialize:function(opts){
   app=opts.app;
   data=app.configure({start:dat}).start;
@@ -60,9 +61,9 @@ export let PackingView=BaseIntView.extend({
   this.setCtr(0);
   this.setLastPhase(3);
 
-  this.next();//TODO:remove
-  this.next();//TODO:remove
-  this.phase2();
+  //this.next();//TODO:remove
+  //this.next();//TODO:remove
+  //this.phase2();//TODO:remove
  },
  anim:function(e){
   if(e.originalEvent.propertyName===data.view.item.fakeTrs)
@@ -75,6 +76,7 @@ export let PackingView=BaseIntView.extend({
     this.setCtr(this.index);
    }else
    {
+    this.$items.eq(this.index).removeClass(this.shownCls+' '+data.view.item.putLCls+' '+data.view.item.putRCls);
     this.setCtr(this.index+1);
     setTimeout(()=>{this.next();this.phase2();},data.beforePhase1);
    }
@@ -87,13 +89,6 @@ export let PackingView=BaseIntView.extend({
    return this.nodeType!==3;
   }));
 
-  this.circles.clicked=0;
-  this.circles.failed=0;
-
-  if(this.phase2Lotties.length)
-   for(let i=0;i<this.phase2Lotties.length;i++)
-    this.phase2Lotties[i].destroy();
-  this.phase2Lotties=[];
   this.$(data.view.lottie.circle).each(function(i){
    setTimeout(()=>{
     let l=lottie.loadAnimation({
@@ -109,6 +104,7 @@ export let PackingView=BaseIntView.extend({
     l.addEventListener('complete',()=>{
      self.$circles.eq(i).removeClass(self.shownCls);
      self.circles.failed++;
+     self.setPoints(data.points.no);
      self.end();
     });
    },data.circles.wait*i);
@@ -117,7 +113,7 @@ export let PackingView=BaseIntView.extend({
  end:function(){
   if(this.circles.clicked+this.circles.failed===data.circles.data.length)
   {
-   app.get('aggregator').trigger('ls:save',{interactive:'1-25',value:'test'});
+   app.get('aggregator').trigger('ls:save',{interactive:'1-25',value:this.points});
    this.next();
   }
  },
@@ -125,7 +121,7 @@ export let PackingView=BaseIntView.extend({
   let circle=$(e.currentTarget).removeClass(this.shownCls);
 
   this.circles.clicked++;
-  //console.log(this.phase2Lotties[this.$circles.index(circle)]);
+  this.setPoints(data.points.yes);
   this.phase2Lotties[this.$circles.index(circle)].pause();
   this.end();
  },
@@ -148,6 +144,22 @@ export let PackingView=BaseIntView.extend({
  setCtr:function(i){
   this.$ctr.text(this.ctrTemplate({curr:i,amt:this.iLength}));
  },
+ setPoints:function(v){
+  data.view.score.cls.forEach((o)=>{
+   this.$dots.removeClass(o);
+  });
+
+  this.points+=v;
+  if(this.points<0)
+   this.points=0;
+  this.$score.text(this.points);
+  if(this.points>=data.points[data.view.score.cls[2]])
+   this.$dots.addClass(data.view.score.cls[2]);else
+    if(this.points>=data.points[data.view.score.cls[1]])
+     this.$dots.addClass(data.view.score.cls[1]);else
+     if(this.points>=data.points[data.view.score.cls[0]])
+      this.$dots.addClass(data.view.score.cls[0]);
+ },
  lHover:function(){
   this.$el.addClass(data.view.item.lCls);
  },
@@ -164,9 +176,11 @@ export let PackingView=BaseIntView.extend({
    {
     this.$items.eq(this.index).addClass(data.view.item.putLCls);
     this.waiting=true;
+    this.setPoints(data.points.yes);
    }else
    {
     app.get('aggregator').trigger('sound','btn');
+    this.setPoints(data.points.no);
    }
   }
  },
@@ -177,22 +191,34 @@ export let PackingView=BaseIntView.extend({
    {
     this.$items.eq(this.index).addClass(data.view.item.putRCls);
     this.waiting=true;
+    this.setPoints(data.points.yes);
    }else
    {
     app.get('aggregator').trigger('sound','btn');
+    this.setPoints(data.points.no);
    }
   }
  },
- /*toggle:function(f){
+ toggle:function(f){
   BaseIntView.prototype.toggle.apply(this,arguments);
- },*/
- click:function(){
-  //this.next();
-
-  /*if(app.get('lib.utils.form.validate')({check:this.$inp,data:data.view.vData,error:(obj)=>obj.addClass(data.view.errCls)}))
+  if(!f)
   {
-   app.get('aggregator').trigger('board:user',Object.fromEntries(this.$inp.serializeArray().map(({name,value})=>[name,value])));
-   this.away();
-  }*/
- }
+   this.waiting=false;
+   this.index=0;
+   this.$items.eq(this.index).addClass(this.shownCls);
+
+   this.points=0;
+   this.circles.clicked=0;
+   this.circles.failed=0;
+
+   for(let i=0;i<this.phase2Lotties.length;i++)
+    this.phase2Lotties[i].destroy();
+   this.phase2Lotties=[];
+   data.view.score.cls.forEach((o)=>{
+    this.$dots.removeClass(o);
+   });
+   this.setCtr(0);
+   this.$score.text(0);
+  }
+ },
 });
